62b3a9d10fe94ccc92d0a8d513f6dcaf1c4cb023
AmirMohamadkhani <90556025+AmirMohamadkhani@users.noreply.github.com>
2021-10-01 00:52:47 +0330

    Update urls.py

--- pay_ir/api/urls.py
+++ pay_ir/api/urls.py
@@ -3,8 +3,8 @@
 """
 
 # Endpoint for sending requests to get a transaction id.
-API_URL_SEND = 'https://pay.ir/payment/send'
+API_URL_SEND = 'https://pay.ir/pg/send'
 # Bank payment page in which user enters his/her card information for payment.
-API_URL_PAYMENT_GATEWAY = 'https://pay.ir/payment/gateway/{trans_id}'
+API_URL_PAYMENT_GATEWAY = 'https://pay.ir/pg/gateway/{trans_id}'
 # Verification and committing transactions endpoint.
-API_URL_VERIFY = 'https://pay.ir/payment/verify'
+API_URL_VERIFY = 'https://pay.ir/pg/verify'
